03a7465e53525bd7a7e0a6a1f9dece19a4ddb8ce
Yik lun Chan <cylunhk0709@gmail.com>
2023-02-08 11:22:04 -0800

    add health and ready

---
+++ flask_app/app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from werkzeug.exceptions import HTTPException
+import redis
+
+app = Flask(__name__)
+
+APP_READY_KEY = "app_ready_key"
+
+
+@app.route("/healthz")
+def health():
+    return { "status": "OK" }
+
+
+@app.route("/readyz")
+def ready():
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    is_ready = int(r.get(APP_READY_KEY)) == 1
+    print(f"after set {is_ready}")
+    if not is_ready:
+        return  { "status": "SERVICE UNAVAILABLE" }
+    return { "status": "OK" }
+
+@app.route("/readyz/enable")
+def enable_traffic():
+    # ping redis here
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r.set(APP_READY_KEY, 1)
+    return {}, 202
+
+@app.route("/readyz/disable")
+def disable_traffic():
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r.set(APP_READY_KEY, 0)
+    return {}, 202
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
