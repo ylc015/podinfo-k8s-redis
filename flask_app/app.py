5d35566d6ed95fe92535be74e93823bbec53c81a
Yik lun Chan <cylunhk0709@gmail.com>
2023-02-08 13:13:31 -0800

    app can listen

--- flask_app/app.py
+++ flask_app/app.py
@@ -10,6 +10,8 @@ import asyncio
 app = Flask(__name__)
 
 APP_READY_KEY = "app_ready_key"
+REDIS_HOST = os.environ.get("REDIS_HOST", 'localhost')
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 
 
 @app.route("/healthz")
@@ -19,7 +21,7 @@ def health():
 
 @app.route("/readyz")
 def ready():
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
     is_ready = int(r.get(APP_READY_KEY)) == 1
     print(f"after set {is_ready}")
     if not is_ready:
@@ -29,13 +31,13 @@ def ready():
 @app.route("/readyz/enable")
 def enable_traffic():
     # ping redis here
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
     r.set(APP_READY_KEY, 1)
     return {}, 202
 
 @app.route("/readyz/disable")
 def disable_traffic():
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
     r.set(APP_READY_KEY, 0)
     return {}, 202
 
@@ -66,7 +68,7 @@ def handle_exception(e):
 
 @app.route("/cache/<key>", methods=["GET", "POST", "PUT", "DELETE"])
 def cache(key: str):
-    r = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, charset="utf-8", decode_responses=True)
     if request.method == "GET":
         data = str(r.get(key))
         if data == 'None':
